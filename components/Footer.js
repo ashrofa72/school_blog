3b12d342abe819b8aa701113a86553b89b89a0d0
Ashraf <as.ka1@hotmail.com>
2023-05-29 19:34:04 +0300

    28 commit

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-fuchsia-800 py-4">
+    <footer className=" py-4" style={{ backgroundColor: '#00337C' }}>
       <div className="container mx-auto text-white text-center">
         <p className="text-sm">
           Copyright © 2023 - All rights reserved-Ashraf DC
