4a25d96d34a529db946f3db47738b1fbf81c4faa
Ashraf <as.ka1@hotmail.com>
2023-05-29 18:56:33 +0300

    27 commit

--- pages/students/Secondcodes.js
+++ pages/students/Secondcodes.js
@@ -6,6 +6,7 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
 
 const Firstcodes = () => {
   const [students, setStudents] = useState([]);
@@ -31,9 +32,19 @@ const Firstcodes = () => {
 
   return (
     <div class="container text-center" style={{ height: '80vh' }}>
-      <h1 className="display-5 p-3">بيانات طلاب الصف الثاني</h1>
-      <div className="row row-cols-12">
-        {/* <div className='col-6'>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <h1 className="display-5 p-3">بيانات طلاب الصف الثاني</h1>
+        <h2>ابحث هنا بالرقم القومي للحصول على بيانات الطالبة</h2>
+        <div className="row row-cols-12">
+          {/* <div className='col-6'>
                     <form>
                         <input className='text-center' type='text'
                             onChange={(e) => setStudentId(e.target.value)}
@@ -62,28 +73,28 @@ const Firstcodes = () => {
                         ))}
                     </div>
                         </div>*/}
-        <div className="col-12">
-          <form>
-            <Stack spacing={2} direction="row" justifyContent="center">
-              <input
-                className="text-center"
-                type="text"
-                onChange={(e) => setStudentId(e.target.value)}
-                value={studentId}
-                placeholder="Student National ID"
-              />
+          <div className="col-12">
+            <form>
+              <Stack spacing={2} direction="row" justifyContent="center">
+                <input
+                  className="text-center"
+                  type="text"
+                  onChange={(e) => setStudentId(e.target.value)}
+                  value={studentId}
+                  placeholder="Student National ID"
+                />
 
-              <Button
-                variant="contained"
-                className="text-white"
-                onClick={handleSearchId}
-              >
-                Search
-              </Button>
-            </Stack>
-          </form>
+                <Button
+                  variant="contained"
+                  className="text-white"
+                  onClick={handleSearchId}
+                >
+                  Search
+                </Button>
+              </Stack>
+            </form>
 
-          {/* <div>
+            {/* <div>
                         {students.map(student => (
                             <div className='my-4 mx-4'>
                                 <ul className='list-unstyled'>
@@ -101,52 +112,61 @@ const Firstcodes = () => {
 
                         ))}
                         </div>*/}
-          <div>
-            {students.map((student) => (
-              <Card
-                key={student.id}
-                variant="outlined"
-                sx={{
-                  minWidth: 275,
-                  my: 2,
-                  ml: 5,
-                }}
-              >
-                <CardContent>
-                  <Typography
-                    sx={{ fontSize: 20 }}
-                    color="text.dark"
-                    gutterBottom
+            <Box sx={{ width: '25%' }}>
+              <div class="col-4">
+                {students.map((student) => (
+                  <Card
+                    key={student.id}
+                    variant="outlined"
+                    sx={{
+                      minWidth: 275,
+                      my: 2,
+                      ml: 5,
+                    }}
                   >
-                    بيانات الطالبة
-                  </Typography>
-                  <Typography variant="h5" component="div" fontWeight="bold">
-                    <h3>اسم الطالبة ({student.stname})</h3>
-                  </Typography>
-                  <Typography
-                    sx={{ mb: 1.5 }}
-                    color="text.secondary"
-                    fontWeight="bold"
-                  >
-                    <p>كود الطالب {student.stcode}</p>
-                    <p> الرقم القومي {student.stnationalid}</p>
-                    <p>تاريخ الميلاد {student.stbirthdate}</p>
-                  </Typography>
-                  <Typography variant="body2" fontWeight="bold">
-                    <p>{student.stcode}@qena1.moe.edu.eg</p>
-                    <p>كلمة المرور Std#{student.stnationalid.slice(1, 7)}</p>
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button variant="success" size="small">
-                    Learn More
-                  </Button>
-                </CardActions>
-              </Card>
-            ))}
+                    <CardContent>
+                      <Typography
+                        sx={{ fontSize: 20 }}
+                        color="text.dark"
+                        gutterBottom
+                      >
+                        بيانات الطالبة
+                      </Typography>
+                      <Typography
+                        variant="h5"
+                        component="div"
+                        fontWeight="bold"
+                      >
+                        <h3>اسم الطالبة ({student.stname})</h3>
+                      </Typography>
+                      <Typography
+                        sx={{ mb: 1.5 }}
+                        color="text.secondary"
+                        fontWeight="bold"
+                      >
+                        <p>كود الطالب {student.stcode}</p>
+                        <p> الرقم القومي {student.stnationalid}</p>
+                        <p>تاريخ الميلاد {student.stbirthdate}</p>
+                      </Typography>
+                      <Typography variant="body2" fontWeight="bold">
+                        <p>{student.stcode}@qena1.moe.edu.eg</p>
+                        <p>
+                          كلمة المرور Std#{student.stnationalid.slice(1, 7)}
+                        </p>
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button variant="success" size="small">
+                        Learn More
+                      </Button>
+                    </CardActions>
+                  </Card>
+                ))}
+              </div>
+            </Box>
           </div>
         </div>
-      </div>
+      </Box>
     </div>
   );
 };
