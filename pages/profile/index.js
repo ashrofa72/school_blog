beb7ab15b7b970fa4b60ed0ddbaede0ec24da6f5
Ashraf <as.ka1@hotmail.com>
2023-05-30 18:11:09 +0300

    36 commit

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -8,12 +8,6 @@ const Profile = () => {
   return (
     <div className={styles.container}>
       <h1>Profile Page</h1>
-      <p>
-        User Creation Date :{' '}
-        {user ? auth.currentUser.metadata.creationTime : 'no user'}
-      </p>
-      <p>User Email : {user ? auth.currentUser.email : 'no user'}</p>
-      <p>Mobile Number : {user ? auth.currentUser.phoneNumber : 'no user'}</p>
     </div>
   );
 };
